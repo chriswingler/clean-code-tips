ea0dcdd944341a5e4110cdfeb994989141deee7d
Chris Wingler <35403641+chriswingler@users.noreply.github.com>
2019-10-26 17:30:05 -0700

    format / clean / etc

--- vscode-extension/src/extension.ts
+++ vscode-extension/src/extension.ts
@@ -1,79 +1,86 @@
 import * as vscode from 'vscode';
 import * as cleanCodeTips from '../../cleancodecheatsheet2.4.json';
 
-type categories = { [key: string]: { [key: string]: { [key: string]: string } } };
+type categories = {
+  [key: string]: { [key: string]: { [key: string]: string } };
+};
 
-const recurseThroughTree = (categories: any | { text: string, type: string }, outputString: string = '', i: number = 0): string => {
+const recurseThroughTree = (
+  categories: any | { text: string; type: string },
+  outputString: string = '',
+  i: number = 0
+): string => {
   if (i < 2) {
     const categoryKeys: Array<string> = Object.keys(categories);
-    const randomCategoryIndex: number = categoryKeys.indexOf(categoryKeys[Math.floor(categoryKeys.length * Math.random())]);
+    const randomCategoryIndex: number = categoryKeys.indexOf(
+      categoryKeys[Math.floor(categoryKeys.length * Math.random())]
+    );
     const randomCategoryName: string = categoryKeys[randomCategoryIndex];
     const randomCategories: any = categories[randomCategoryName];
 
     if (i === 0) {
-
       outputString += `${randomCategoryName} `;
 
-      switch(randomCategoryName) {
-        case "Principles":
-          outputString += "🗽";
+      switch (randomCategoryName) {
+        case 'Principles':
+          outputString += '🗽';
           break;
-        case "Smells":
-          outputString += "💩";
+        case 'Smells':
+          outputString += '💩';
           break;
-        case "Class Design":
-          outputString += "🧱";
+        case 'Class Design':
+          outputString += '🧱';
           break;
-        case "Package Cohesion":
-          outputString += "📦";
+        case 'Package Cohesion':
+          outputString += '📦';
           break;
-        case "Package Coupling":
-          outputString += "🧑‍🤝‍🧑";
+        case 'Package Coupling':
+          outputString += '🧑‍🤝‍🧑';
           break;
-        case "General":
-          outputString += "📖";
+        case 'General':
+          outputString += '📖';
           break;
-        case "Environment":
-          outputString += "🌎";
+        case 'Environment':
+          outputString += '🌎';
           break;
-        case "Dependency Injection":
-          outputString += "💉";
+        case 'Dependency Injection':
+          outputString += '💉';
           break;
-        case "Design":
-          outputString += "✍";
+        case 'Design':
+          outputString += '✍';
           break;
-        case "Dependencies":
-          outputString += "👨‍👧‍👦";
+        case 'Dependencies':
+          outputString += '👨‍👧‍👦';
           break;
-        case "Naming":
-          outputString += "🏷";
+        case 'Naming':
+          outputString += '🏷';
           break;
-        case "Understandability":
-          outputString += "📖";
+        case 'Understandability':
+          outputString += '📖';
           break;
-        case "Methods":
-          outputString += "🏃";
+        case 'Methods':
+          outputString += '🏃';
           break;
-        case "Source Code Structure":
-          outputString += "🏗";
+        case 'Source Code Structure':
+          outputString += '🏗';
           break;
-        case "Conditionals":
-          outputString += "👈👉";
+        case 'Conditionals':
+          outputString += '👈👉';
           break;
-        case "Useless Stuff":
-          outputString += "🗑";
+        case 'Useless Stuff':
+          outputString += '🗑';
           break;
-        case "Maintainability Killers":
-          outputString += "🔧";
+        case 'Maintainability Killers':
+          outputString += '🔧';
           break;
-        case "Exception Handling":
-          outputString += "🚸";
+        case 'Exception Handling':
+          outputString += '🚸';
           break;
-        case "How to Learn Clean Code":
-          outputString += "👨‍🏫";
+        case 'How to Learn Clean Code':
+          outputString += '👨‍🏫';
           break;
-        case "Refactoring Patterns":
-          outputString += "🔨";
+        case 'Refactoring Patterns':
+          outputString += '🔨';
           break;
       }
     } else {
@@ -95,19 +102,19 @@ const recurseThroughTree = (categories: any | { text: string, type: string }, ou
   }
 };
 
-const rootDataObj: categories = cleanCodeTips["Clean Code Cheat Sheet"];
+const rootDataObj: categories = cleanCodeTips['Clean Code Cheat Sheet'];
 
 export function activate({ subscriptions }: vscode.ExtensionContext) {
   let outputString: string = '';
 
-  subscriptions.push(vscode.commands.registerCommand("onCommand:extension.displayTip", () => {
-    outputString = recurseThroughTree(rootDataObj);
-  
-    // Display a message box to the user
-    vscode.window.showInformationMessage(
-      outputString
-    );
-  }));
+  subscriptions.push(
+    vscode.commands.registerCommand('onCommand:extension.displayTip', () => {
+      outputString = recurseThroughTree(rootDataObj);
+
+      // Display a message box to the user per click
+      vscode.window.showInformationMessage(outputString);
+    })
+  );
 
   // Add status bar item
   const myStatusBarItem: vscode.StatusBarItem = vscode.window.createStatusBarItem(
@@ -115,68 +122,70 @@ export function activate({ subscriptions }: vscode.ExtensionContext) {
     100
   );
 
-  myStatusBarItem.text = "Clean Code Tips";
-  
+  myStatusBarItem.text = 'Clean Code Tips';
+
   // Make a tip show when clicking on status bar item
-  myStatusBarItem.command = "onCommand:extension.displayTip";
+  myStatusBarItem.command = 'onCommand:extension.displayTip';
 
   outputString = recurseThroughTree(rootDataObj);
 
   // Display a message box to the user on startup
-  vscode.window.showInformationMessage(
-    outputString
-    // 'Principles 🗽 > High Cohesion: Cohesion is the degree to which elements of a whole belong together. \nMethods and fields in a single class and classes of a component should have \nhigh cohesion. High cohesion in classes and components results in simpler, \nmore easily understandable code structure and design. \n'
-  );
-	subscriptions.push(myStatusBarItem);
+  vscode.window.showInformationMessage(outputString);
+  subscriptions.push(myStatusBarItem);
   myStatusBarItem.show();
-  
+
   // Shorthand
-  const displayTip = () => vscode.commands.executeCommand('extension.displayTip');
+  const displayTip = () =>
+    vscode.commands.executeCommand('extension.displayTip');
 
   // Add settings page
   const tipTimer = vscode.workspace.getConfiguration().get('tipTimer');
 
-	switch (tipTimer) {
-		case '5 minutes':
+  // subscriptions.push(tipTimer)
+
+  switch (tipTimer) {
+    case '5 minutes':
       displayTip();
-			break;
-		case '10 minutes':
+      break;
+    case '10 minutes':
       displayTip();
-			break;
-		case '15 minutes':
+      break;
+    case '15 minutes':
       displayTip();
-			break;
-		case '30 minutes':
+      break;
+    case '30 minutes':
       displayTip();
-			break;
-		case '1 hour':
+      break;
+    case '1 hour':
       displayTip();
-			break;
-		case '2 hours':
+      break;
+    case '2 hours':
       displayTip();
-			break;
-		case '4 hours':
+      break;
+    case '4 hours':
       displayTip();
-			break;
-		case '8 hours':
+      break;
+    case '8 hours':
       displayTip();
-			break;
-		case '1 day':
+      break;
+    case '1 day':
       displayTip();
-			break;
-		case '1 week':
+      break;
+    case '1 week':
       displayTip();
-			break;
-		case '1 month':
+      break;
+    case '1 month':
       displayTip();
-			break;
+      break;
   }
-  
-  const testingToggle = vscode.workspace.getConfiguration().get('tipsForTestingCode');
-	switch (testingToggle) {
-		case true:
-			break;
-		case false:
-			break;
-	}
+
+  const testingToggle = vscode.workspace
+    .getConfiguration()
+    .get('tipsForTestingCode');
+  switch (testingToggle) {
+    case true:
+      break;
+    case false:
+      break;
+  }
 }
