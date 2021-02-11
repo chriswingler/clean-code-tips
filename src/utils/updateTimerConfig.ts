37a13f9ede4e71b4f4e9c943ae4a366fe67b44cf
Chris Wingler <chriswingler1986@gmail.com>
2021-02-11 02:07:24 -0800

    del old files

--- src/utils/updateTimerConfig.ts
+++
@@ -1,8 +0,0 @@
-import * as vscode from 'vscode';
-
-const updateTimerConfig = async () => {
-  const config = vscode.workspace.getConfiguration();
-  await config.update('tipTimer', config, vscode.ConfigurationTarget.Global);
-};
-
-export default updateTimerConfig;
